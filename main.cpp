9dbc3a0e50befea3d5441a49abfc9eb6a8fbbe30
Babanov Fedor <2000.fedor@mail.ru>
2022-10-24 18:20:14 +0500

    7th commit

--- main.cpp
+++ main.cpp
@@ -4,10 +4,11 @@ using namespace std;
 
 int main()
 {
-    int x, y, sum;
+    int x, y, sum, dif;
 
     cout << "Enter x: " << endl; cin >> x;
     cout << "Enter y: " << endl; cin >> y;
-    cout << "Sum: " << x+y << endl;
+    cout << "Sum: " << x + y << endl;
+    cout << "Difference: " << x - y << endl;
     return 0;
 }
