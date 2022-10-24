c286da357f9c0167967f1a69c260d41c6eb77935
Babanov Fedor <2000.fedor@mail.ru>
2022-10-24 20:52:28 +0500

    11th commit

--- main.cpp
+++ main.cpp
@@ -14,5 +14,6 @@ int main()
     cout << "Enter y: " << endl; cin >> y;
     cout << "Sum: " << x + y << endl;
     cout << "Difference: " << x - y << endl;
+    cout << "x / y = " << x / y << endl;
     return 0;
 }
